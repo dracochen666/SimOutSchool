5ab2dfa3ba4592deb2cb8e893edd22e730cd8f69
chenlong <2474184221@qq.com>
2022-09-20 18:08:23 +0800

    files

---
+++ SimOutSchool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimOutSchool
+//
+//  Created by 陈龙 on 2022/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("push test")
+    }
+
+
+}
+
